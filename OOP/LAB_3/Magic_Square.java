407aa2b8ac212614cf3c2975759e5c0bb61d9a04
Adarsh Dwivedi <adarshdwivedi121@gmail.com>
2016-08-29 04:45:49 +0000

    Upload new file

---
+++ OOP/LAB_3/Magic_Square.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+class Magic_Square
+{
+	static Scanner sc=new Scanner(System.in);
+
+	static int rowSum(int arr[][], int row, int s)
+	{
+		int sum=0;
+		for(int j=0; j<s; j++)
+			sum+=arr[row][j];
+
+		return sum;
+	}
+
+	static int colSum(int arr[][], int s, int col)
+	{
+		int sum=0;
+		for(int j=0; j<s; j++)
+			sum+=arr[j][col];
+
+		return sum;
+	}
+
+	static int leftDiagSum(int arr[][], int s)
+	{
+		int sum=0;
+		for(int j=0; j<s; j++)
+			sum+=arr[j][j];
+
+		return sum;
+	}
+
+	static int rightDiagSum(int arr[][], int s)
+	{
+		int sum=0;
+		for(int j=0; j<s; j++)
+			sum+=arr[s-j-1][j];
+
+		return sum;
+	}
+
+	public static void main(String[] argc)
+	{
+		
+		System.out.print("\nEnter Size : ");
+		int s=sc.nextInt();
+		
+		int[][] a=new int[s][s];
+		System.out.print("\nEnter Matrix :\n");
+		for(int i=0; i<s; i++)
+			for(int j=0; j<s; j++)
+				a[i][j]=sc.nextInt();
+
+		int frs=0, fcs=0, lds, rds;
+		int[] r=new int[s];
+		int[] c=new int[s];
+
+		for(int i=0; i<s; i++)
+			r[i]=rowSum(a, i, s);
+
+		for(int i=0; i<s; i++)
+			c[i]=colSum(a, s, i);
+
+		lds=leftDiagSum(a, s);
+		rds=rightDiagSum(a, s);
+
+		for(int i=0; i<s-1; i++)
+			if(r[i]!=r[i+1])
+				frs=1;
+
+		for(int i=0; i<s-1; i++)
+			if(c[i]!=c[i+1])
+				fcs=1;
+
+		if(r[0]==c[0] && r[0]==lds && r[0]==rds && frs!=1 && fcs!=1)
+			System.out.print("\nIt is a Magic Square.");
+		else
+			System.out.print("\nIt is not a Magic Square.");
+
+	}
+}
