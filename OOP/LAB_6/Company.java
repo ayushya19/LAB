69a75d627e2c4b572e939a88e35edc436aa4ee84
Adarsh Dwivedi <adarshdwivedi121@gmail.com>
2016-09-26 10:43:51 +0530

    LAB 6 codes added for OOP Lab.

--- OOP/LAB_6/Company.java
+++ OOP/LAB_6/Company.java
@@ -8,71 +8,101 @@ interface HCL{
 }
 
 class Hardware implements HCL{ 
-    String c, m;
+    String c="Camera",
+            m="GoPro";
+    int n;
     Hardware(){
-        c=m="";
+        n=0;
     }
+    Hardware(int a){
+        n=a;
+    }
+    
 
     @Override
     public void input(){
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter Category:");
-        String c=s.next();
-        System.out.println("Enter Manufacture:");
-        String m=s.next();
+        n++;
     }
 
     @Override
     public void print(){
-        System.out.println("Enter Category:" + c);
-        System.out.println("Enter Manufacture:" + m);
+        System.out.println("Category:" + c);
+        System.out.println("Manufacture:" + m);
+        System.out.println("No. of units Sold : " + n);
     }
 }
 
 class Software implements HCL{
-    String t, o;
+    String t="SDK",
+            o="Linux";
+    int n;
     Software(){
-        t=o="";
+        n=0;
+    }
+    Software(int a){   
+        n=a;
     }
 
     @Override
     public void input(){
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter Type:");
-        String t=s.next();
-        System.out.println("Enter OS:");
-        String o=s.next();
+        n++;
     }
 
     @Override
     public void print(){
-        System.out.println("Enter Type:" + t);
-        System.out.println("Enter OS:" + o);
+        System.out.println("Type:" + t);
+        System.out.println("OS:" + o);
+        System.out.println("No. of units Sold : " + n);
     }
 }
 
 class Company{
     public static void main(String arg[]){
         Scanner sc=new Scanner(System.in);
-        Hardware h=new Hardware();
-        Software s=new Software();
-        int a=3,ch=0,cs=0;
-        while((a==1)||(a==2)||(a==3)){
-            System.out.println("1.Hardware \n2.Software");
-            a=sc.nextInt();
-            if(a==1)
-            {
-                h.input();
-                h.print();
-                ch++;
-            }							 
-            else if(a==2)
-            {
-                s.input();
-                s.print();
-                cs++;
-            }
+        Hardware h[]=new Hardware[12];
+        Software s[]=new Software[12];
+        int a=0, m=0;
+        
+        for(int i=0; i<12; i++)
+        {
+            h[i]=new Hardware();
+            s[i]=new Software();
         }
-        System.out.println("Sales for: Hardware:" +ch +"Software:" +cs);
+        
+        while(a!=4){
+            System.out.println("1.Hardware \n2.Software \n3.Sales Record");
+            a=sc.nextInt();
+            
+            switch (a)
+            {
+                case 1: System.out.println("Enter the Month Hardware was Sold.");
+                        m=sc.nextInt();
+                        h[m-1].input();                
+                        break;
+            							 
+                case 2: System.out.println("Enter the Month Hardware was Sold.");
+                        m=sc.nextInt();
+                        s[m-1].input();
+                        break;
+                        
+                case 3: System.out.println("Enter the Month");
+                        m=sc.nextInt();
+                        h[m].print();
+                        s[m].print();
+                        break;
+            }            
+        }
+        System.out.println("Sales of Last 3 Months : ");
+        int hn=0,sn=0;
+        for(int i=9; i<12; i++)
+        {
+            hn+=h[i].n;
+            sn+=s[i].n;
+        }
+
+        Hardware h1=new Hardware(hn);
+        h1.print();
+        Software s1=new Software(sn);
+        s1.print();
     }
 }
